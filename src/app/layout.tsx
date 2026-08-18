f3fa97395ab8a4c5b8e0ec3908247c084b765e61
서혜진 <hjseo@laonpeople.com>
2026-08-18 13:35:23 +0900

    Feat: 기본 반응형 레이아웃 생성 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
+import { ResponsiveLayout } from "@/shared/ui/layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <ResponsiveLayout>{children}</ResponsiveLayout>
+      </body>
     </html>
   );
 }
